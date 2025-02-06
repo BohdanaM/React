3d6d97a6501c9dbca1026bf2744e7580fd74b9b6
Bogdana Matveeva <b.matveeva@epicentrm.com.ua>
2025-02-06 15:22:49 -0800

    Add redux folder

--- counter-redux/src/App.js
+++ counter-redux/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement } from "./actions";
+import { increment, decrement } from "./redux/actions";
 import "./App.css";
 
 const App = () => {
