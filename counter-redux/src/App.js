b0c9e5356519c180f6c608e26580e99977c6d7de
Bogdana Matveeva <b.matveeva@epicentrm.com.ua>
2025-02-06 14:56:51 -0800

    Resolving HW29.1

---
+++ counter-redux/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./actions";
+import "./App.css";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  return (
+    <div className="app-container">
+      <div className="counter-container">
+        <h1 className="counter-title">Counter</h1>
+        <div className="counter">
+          <button
+            className="decrement-btn"
+            onClick={() => dispatch(decrement())}
+          >
+            -
+          </button>
+          <span className="counter-value">{count}</span>
+          <button
+            className="increment-btn"
+            onClick={() => dispatch(increment())}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
